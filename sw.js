0070785ca7b30812809ffbe456ffacc9da3d119d
eliash9 <18025275+eliash9@users.noreply.github.com>
2025-08-13 22:47:09 +0700

    dd

--- sw.js
+++ sw.js
@@ -1,9 +1,9 @@
 const CACHE_NAME = 'simulasi-pembiayaan-v1';
 const urlsToCache = [
   '/',
-  'index.html',
-  'icons/icon-192x192.png',
-  'icons/icon-512x512.png'
+  '/index.html',
+  '/icons/icon-192x192.png',
+  '/icons/icon-512x512.png'
 ];
 
 self.addEventListener('install', event => {
